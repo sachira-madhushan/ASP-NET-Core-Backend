d53381b06541bccd317b7b5e3de5057c3cb2932e
who am i <sachiramadhushan.dev@gmail.com>
2025-01-23 14:41:13 +0530

    Basic API

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using ASP_NET_Core_Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Core_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private static List<Category> categories = new List<Category>() { 
+            new Category(){Id=0,Name="Sachira",ImageUrl="sachira.png"},
+            new Category(){Id=1,Name="Madhushan",ImageUrl="madhushan.png"}
+        };
+
+
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return categories;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Category category)
+        {
+            categories.Add(category);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id,[FromBody]Category category)
+        {
+            categories[id] = category;
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            categories.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP_NET_Core_Backend.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
