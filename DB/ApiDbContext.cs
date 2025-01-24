8f02f9824431acc6d78296d0013927c0b31bf4bc
who am i <sachiramadhushan.dev@gmail.com>
2025-01-24 10:30:15 +0530

    Stash Updated

---
+++ DB/ApiDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ASP_NET_Core_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_NET_Core_Backend.DB
+{
+    public class ApiDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=SocialMedia;");
+        }
+
+    }
+}
